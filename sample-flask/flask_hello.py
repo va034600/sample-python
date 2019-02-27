e071b58ccd9cc5c690ae38024247e12078660980
va034600 <va034600@gmail.com>
2019-02-27 22:25:23 +0900

    sample flask json

--- sample-flask/flask_hello.py
+++ sample-flask/flask_hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def hello_world():
     return 'ハローワールド!!'
+
+
+@app.route('/hello_world_json', methods=['GET'])
+def hello_world_json():
+    return jsonify({"aa": "bb"})
 
 
 if __name__ == '__main__':
--- sample-flask/test_flask_hello.py
+++ sample-flask/test_flask_hello.py
@@ -1,5 +1,6 @@
 import unittest
 import flask_hello
+import ast
 
 
 class TestFlaskHello(unittest.TestCase):
@@ -10,6 +11,13 @@ class TestFlaskHello(unittest.TestCase):
         response = self.app.get('/')
         assert response.status_code == 200
         assert response.data.decode() == 'ハローワールド!!'
+
+    def test_hello_world_json(self):
+        response = self.app.get('/hello_world_json')
+        a = ast.literal_eval(response.data.decode())
+        b = {"aa": "bb"}
+        assert all((k, v) in a.items()
+                   for (k, v) in b.items())
 
 
 if __name__ == '__main__':
