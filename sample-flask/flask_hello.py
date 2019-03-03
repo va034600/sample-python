e7dfeaceff5f5e7d76b0ef3a39a6aefa8ef6d49f
va034600 <va034600@gmail.com>
2019-03-03 10:15:00 +0900

    sample flask json

--- sample-flask/flask_hello.py
+++ sample-flask/flask_hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -11,6 +11,35 @@ def hello_world():
 @app.route('/hello_world_json', methods=['GET'])
 def hello_world_json():
     return jsonify({"aa": "bb"})
+
+
+@app.route('/hello_world_get_parameter/<path_variable>', methods=['GET'])
+def hello_world_get_parameter(path_variable=None):
+    return jsonify({
+        "path_variable": path_variable,
+        "parameter_variable": request.args.get('parameter_variable')
+    })
+
+
+@app.route('/hello_world_post_parameter/<path_variable>', methods=['POST'])
+def hello_world_post_parameter(path_variable=None):
+    return jsonify({
+        "path_variable": path_variable,
+        "parameter_variable": request.form['parameter_variable']
+    })
+
+
+@app.route('/hello_world_post_parameter_json/<path_variable>', methods=['POST'])
+def hello_world_post_parameter_json(path_variable=None):
+    if request.headers['Content-Type'] != 'application/json':
+        return jsonify(status='error'), 400
+
+    print(request.get_json()['parameter_variable'])
+
+    return jsonify({
+        "path_variable": path_variable,
+        "parameter_variable": request.get_json()['parameter_variable']
+    })
 
 
 if __name__ == '__main__':
--- sample-flask/test_flask_hello.py
+++ sample-flask/test_flask_hello.py
@@ -19,6 +19,49 @@ class TestFlaskHello(unittest.TestCase):
         assert all((k, v) in a.items()
                    for (k, v) in b.items())
 
+    def test_hello_world_get_parameter(self):
+        response = self.app.get('/hello_world_get_parameter/aaa?parameter_variable=bbb')
+        a = ast.literal_eval(response.data.decode())
+        b = {
+            "path_variable": "aaa",
+            "parameter_variable": "bbb"
+        }
+        assert all((k, v) in a.items()
+                   for (k, v) in b.items())
+
+    def test_hello_world_post_parameter(self):
+        response = self.app.post('/hello_world_post_parameter/aaa', data=dict(parameter_variable="bbb"))
+        a = ast.literal_eval(response.data.decode())
+        b = {
+            "path_variable": "aaa",
+            "parameter_variable": "bbb"
+        }
+        assert all((k, v) in a.items()
+                   for (k, v) in b.items())
+
+    def test_hello_world_post_parameter_json(self):
+        response = self.app.post('/hello_world_post_parameter_json/aaa',
+                                 json={"parameter_variable": "bbb"},
+                                 content_type="application/json")
+        a = ast.literal_eval(response.data.decode())
+        b = {
+            "path_variable": "aaa",
+            "parameter_variable": "bbb"
+        }
+        assert all((k, v) in a.items()
+                   for (k, v) in b.items())
+
+    def test_hello_world_post_parameter_json_error(self):
+        response = self.app.post('/hello_world_post_parameter_json/aaa',
+                                 json={"parameter_variable": "bbb"},
+                                 content_type="application/x-www-form-urlencoded")
+        a = ast.literal_eval(response.data.decode())
+        b = {
+            "status": "error"
+        }
+        assert all((k, v) in a.items()
+                   for (k, v) in b.items())
+
 
 if __name__ == '__main__':
     unittest.main()
